d32df9bd0abc2f7657fd1f329b821ece4149d95d
goll <joaog.bastos@protonmail.ch>
2026-05-21 13:23:36 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,56 @@
+CarbonoZero
+===========
+
+CarbonoZero é um sistema fictício que permite monitorar
+emissões de carbono por parte de empresas, permitindo
+gerenciar análises de emissão, que são realizadas por
+"instituições científicas", e aplicar regras de incentivo
+fiscal ou penalização (multas) às empresas, de acordo
+com os resultados obtidos.
+
+## Uso
+
+As dependências necessárias são as seguintes:
+
+ - PostgreSQL 18
+ - Python 3.12
+
+Outras versões podem ser usadas, porém não há garantia de
+compatibilidade.
+
+### _Workflow_ padrão: nix
+
+O _workflow_ recomendado consiste em usar um _nix devshell_
+para instalar todas as dependências e configurar o banco
+de dados automaticamente:
+
+```sh
+nix develop .
+```
+
+A seguir, algumas alternativas são mostradas. Porém, é importante
+ressaltar que ambas as alternativas exigem que o Python e o
+PostgreSQL sejam instalados e configurados manualmente.
+
+As etapas do processo de configuração do PostgreSQL se encontram
+no `shellHook` do `flake.nix`.
+
+### Alternativa: uv
+
+É possível fazer a instalação de dependências Python
+manualmente, usando o `uv`:
+
+```sh
+uv sync
+. .venv/bin/activate
+```
+
+### Alternativa: pip
+
+O `pip` também pode ser usado, embora não seja recomendado:
+
+```sh
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -e .
+```
---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Monitoramento de emissões de carbono corporativas";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    lefthook-nix.url = "github:sudosubin/lefthook.nix";
+    lefthook-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = ["x86_64-linux"];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        py = pkgs.python313Packages;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            py.python
+
+            pkgs.uv
+            pkgs.ty
+            pkgs.ruff
+
+            pkgs.postgresql_18
+          ];
+
+          shellHook = ''
+            REPO_ROOT=$(git rev-parse --show-toplevel)
+
+            cd "$REPO_ROOT"
+            
+            uv sync
+            . .venv/bin/activate
+
+            mkdir -p db/logs
+
+            LOG="db/logs/$(date +'%Y%m%dT%H%M%S.log')"
+            
+            if ! [ -e "$PGDATA" ]; then
+              pg_ctl -l "$LOG" initdb
+
+              ln -sf ../postgresql.conf "$PGDATA/postgresql.conf"
+              pg_ctl -l "$LOG" start
+              
+              PGUSER= psql -d postgres \
+                -c "CREATE USER $PGUSER;" \
+                -c "CREATE DATABASE $PGDATABASE;" \
+                -c "GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSER;"
+
+              PGUSER= psql -c "GRANT ALL PRIVILEGES ON SCHEMA public TO $PGUSER;"
+            else
+              ln -sf ../postgresql.conf "$PGDATA/postgresql.conf"
+              pg_ctl -l "$LOG" restart
+            fi
+          '';
+
+          # Use Unix socket instead of TCP connection
+          PGHOST = "/tmp";
+          # PostgreSQL Data directory
+          PGDATA = "db/datadir";
+          
+          PGUSER = "carbonozero";
+          PGDATABASE = "carbonozero";
+        };
+      };
+    };
+}
