edb6b78b1f7ca75281a5a6216ec027de5c0ee6bc
goll <joaog.bastos@protonmail.ch>
2026-06-17 14:57:46 -0300

    Add some query ideas

---
+++ db/queries.sql
@@ -0,0 +1,25 @@
+-- N filiais com o menor histórico de emissão de carbono nos últimos K meses.
+
+-- Todas as filiais que contribuíram mais do que R$ 500.000,00 em ações de compensação
+-- nos últimos K meses, sendo pelo menos 30% de suas contribuições na área de energia.
+
+-- (por favor não me acuse de plágio)
+-- Todas as filiais que não receberam nenhuma multa no último ano.
+
+-- Todas as filiais que possuem dois ou menos relatórios sobre si gerados no último ano.
+
+-- Todas as filiais que foram investigadas por três ou mais instituições científicas
+-- diferentes nos últimos dois anos.
+
+-- Todas as filiais com saldo de emissão positivo nos últimos Y anos, incluindo o valor total
+-- investido em cada área das ações de compensação.
+
+-- (complexa)
+-- Todas as filiais cujas emissões nos últimos X meses foram oriundas majoritariamente (> 50%)
+-- de um único tipo de produto ou serviço.
+
+-- Para todas as Instituições Cientificas do Rio de Janeiro, listar a média do histórico
+-- de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
+
+-- Todas as regras legislativas que se aplicam a um certo município.
+
