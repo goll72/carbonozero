33f4e437c0f5ecb450da860c686be43369f90f31
goll <joaog.bastos@protonmail.ch>
2026-06-18 13:45:23 -0300

    More query ideas

--- db/queries.sql
+++ db/queries.sql
@@ -3,7 +3,6 @@
 -- Todas as filiais que contribuíram mais do que R$ 500.000,00 em ações de compensação
 -- nos últimos K meses, sendo pelo menos 30% de suas contribuições na área de energia.
 
--- (por favor não me acuse de plágio)
 -- Todas as filiais que não receberam nenhuma multa no último ano.
 
 -- Todas as filiais que possuem dois ou menos relatórios sobre si gerados no último ano.
@@ -21,10 +20,20 @@
 -- Para todas as Instituições Cientificas do Rio de Janeiro, listar a média do histórico
 -- de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
 
+-- (útil)
 -- Todas as regras legislativas que se aplicam a um certo município.
+
+-- (útil)
+-- Todas as regras legislativas que se aplicam a uma determinada filial, em um determinado mês.
 
 -- Todas as empresas que financiaram alguma ação de compensação de uma determinada
 -- organização socioambiental, incluindo o valor total contribuído por cada empresa.
 
 -- As top K filiais que mais investiram em ações de compensação, agrupadas por
 -- área da ação.
+
+-- Todas as regras legislativas de incentivo fiscal cujas metas não foram atingidas por
+-- nenhuma filial nos últimos X meses.
+
+-- Todas as regras legislativas de incentivo fiscal cujas metas foram alcançadas por pelo
+-- menos uma filial todo mês, em um período de três meses subsequentes.
