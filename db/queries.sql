f4846ef21aedebd4af6718766c382bac45b1e410
conti <contimarcelao@gmail.com>
2026-06-19 19:30:45 -0300

    primeira versão filial sem multa em um periodo

--- db/queries.sql
+++ db/queries.sql
@@ -46,3 +46,24 @@ PREPARE reg_leg_mun(TEXT, CHAR(2)) AS
 
 -- Todas as regras legislativas de incentivo fiscal cujas metas foram alcançadas por pelo
 -- menos uma filial todo mês, em um período de três meses subsequentes.
+
+-- 1) Todas as filiais que não receberam nenhuma multa em um intervalo
+PREPARE filial_sem_multa_p(DATE, DATE)
+    SELECT cnpj_raiz, cnpj_ordem FROM filial 
+        EXCEPT 
+        SELECT 
+            f.cnpj_raiz, 
+            f.cnpj_ordem
+        FROM filial AS f
+            JOIN relatorio AS r 
+                ON f.cnpj_raiz = r.cnpj_filial_raiz 
+                AND f.cnpj_ordem = r.cnpj_filial_ordem
+        WHERE r.dt_pedido BETWEEN $1 AND $2 AND r.multa_aplic > 0 
+
+-- 2) Todas as filiais que contribuíram mais do que $"R$X"$ em ações de compensação, nos últimos K meses, sendo pelo menos Y% de suas contribuições em uma area Z
+
+-- 3) Todas as filiais cujas emissões nos últimos X meses foram oriundas majoritariamente de um único tipo de produto ou serviço. -- Implementar
+
+-- 4) Todas as regras legislativas de incentivo fiscal cujas metas não foram atingidas por nenhuma filial nos últimos X meses
+
+-- 5) Para todas as Instituições Cientificas do município X, listar a média do histórico de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
