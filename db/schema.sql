89e099ed7a78a80e93f6fe658e6483112aa2c648
ReCaat <renanbcatarin@gmail.com>
2026-06-19 18:54:45 -0300

    schema constraints added

--- db/schema.sql
+++ db/schema.sql
@@ -1,58 +1,115 @@
+CREATE DOMAIN CNPJ_TIPO AS CHAR(18)
+CHECK (
+    VALUE ~ '^[0-9]{2}\.[0-9]{3}\.[0-9]{3}/[0-9]{4}-[0-9]{2}$'
+);
+
+CREATE DOMAIN COD_IBGE_TIPO AS CHAR(7)
+CHECK (VALUE ~ '^[0-9]{7}$');
+
 CREATE TYPE ENT_FED_TIPO AS ENUM ('mun', 'uf');
 
 CREATE TABLE ent_fed (
-    cod_ibge CHAR(7) PRIMARY KEY,
-    tipo ENT_FED_TIPO
+    cod_ibge COD_IBGE_TIPO,
+    tipo ENT_FED_TIPO,
+
+    CONSTRAINT enf_fed_pk
+        PRIMARY KEY (cod_ibge),
 );
 
 CREATE TABLE uf (
-    sigla CHAR(2) PRIMARY KEY,
-    cod_ibge CHAR(7) NOT NULL UNIQUE REFERENCES ent_fed(cod_ibge) DEFERRABLE
+    sigla CHAR(2),
+    cod_ibge COD_IBGE_TIPO NOT NULL,
+    
+    CONSTRAINT uf_pk
+        PRIMARY KEY(sigla),
+    CONSTRAINT uf_sk
+        UNIQUE (cod_ibge),
+    CONSTRAINT uf_ck
+        CHECK (sigla ~ '^[A-Z]{2}$'),
+    CONSTRAINT uf_fk 
+        FOREIGN KEY (cod_ibge) REFERENCES ent_fed(cod_ibge) 
+        DEFERRABLE
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE org_adm_mun (
-    cod_ibge CHAR(7) PRIMARY KEY REFERENCES ent_fed(cod_ibge) DEFERRABLE,
-    cnpj CHAR(18) NOT NULL UNIQUE,
+    cod_ibge COD_IBGE_TIPO,
+    cnpj CNPJ_TIPO NOT NULL,
     raz_soc TEXT,
     end_rua TEXT,
     end_nro INT,
     end_cep CHAR(9),
     nome_mun TEXT NOT NULL,
-    sigla_uf CHAR(2) REFERENCES uf(sigla)
+    sigla_uf CHAR(2),
+
+    CONSTRAINT org_adm_mun_pk
+        PRIMARY KEY (cod_ibge),
+    CONSTRAINT org_adm_mun_sk
+        UNIQUE (cnpj),
+    CONSTRAINT org_adm_mun_fk_cod
+        FOREIGN KEY (cod_ibge) REFERENCES ent_fed(cod_ibge) 
+        DEFERRABLE
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    CONSTRAINT org_adm_mun_fk_uf
+        FOREIGN KEY (sigla_uf) REFERENCES uf(sigla)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
 );
 
 CREATE TABLE empresa (
-    cnpj_raiz CHAR(10) PRIMARY KEY,
+    cnpj_raiz CHAR(10),
     cnae CHAR(9),
     dt_fund DATE NOT NULL,
     raz_soc VARCHAR(255),
     nat_jur CHAR(5) NOT NULL,
-    dt_adesao DATE NOT NULL
+    dt_adesao DATE NOT NULL,
+
+    CONSTRAINT empresa_pk
+        PRIMARY KEY (cnpj_raiz)
 );
 
 CREATE TABLE filial (
-    cnpj_raiz CHAR(10) REFERENCES empresa(cnpj_raiz),
+    cnpj_raiz CHAR(10),
     cnpj_ordem CHAR(7),
     qtde_func INT,
     end_rua TEXT,
     end_nro INT,
     end_cep CHAR(9),
-    mun_cod CHAR(7) NOT NULL REFERENCES org_adm_mun(cod_ibge),
+    mun_cod COD_IBGE_TIPO NOT NULL,
 
-    PRIMARY KEY (cnpj_raiz, cnpj_ordem)
+    CONSTRAINT filial_pk
+        PRIMARY KEY (cnpj_raiz, cnpj_ordem),
+    CONSTRAINT filial_fk_empresa
+        FOREIGN KEY (cnpj_raiz) REFERENCES empresa(cnpj_raiz)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    CONSTRAINT filial_fk_mun 
+        FOREIGN KEY (mun_cod) REFERENCES org_adm_mun(cod_ibge)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE organiz_socioamb (
-    cnpj CHAR(18) PRIMARY KEY,
+    cnpj CNPJ_TIPO,
     nome TEXT,
     end_rua TEXT,
     end_nro INT,
     end_cep CHAR(9),
-    mun_cod CHAR(7) REFERENCES org_adm_mun(cod_ibge)
+    mun_cod COD_IBGE_TIPO,
+
+    CONSTRAINT organiz_socioamb_pk
+        PRIMARY KEY(cnpj),
+    CONSTRAINT organiz_socioamb_fk
+        FOREIGN KEY(mun_cod) REFERENCES org_adm_mun(cod_ibge)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+
 );
 
 CREATE TABLE acao_co2 (
-    cnpj CHAR(18),
+    cnpj CNPJ_TIPO,
     dt_inicio DATE,
     nome TEXT,
     razao_comp_custo DECIMAL,
@@ -64,29 +121,41 @@ CREATE TABLE acao_co2 (
     p_co2_recic INT,
     p_custo_recic INT,
 
-    PRIMARY KEY (cnpj, dt_inicio, nome),
-    FOREIGN KEY (cnpj) REFERENCES organiz_socioamb(cnpj)
+    CONSTRAINT acao_co2_pk
+        PRIMARY KEY (cnpj, dt_inicio, nome),
+    CONSTRAINT acao_co2_fk
+        FOREIGN KEY (cnpj) REFERENCES organiz_socioamb(cnpj)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 -- MRel: Financia
 CREATE TABLE vinc_contrib_co2 (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     cnpj_filial_raiz CHAR(10) NOT NULL,
     cnpj_filial_ordem CHAR(7) NOT NULL,
-    cnpj_organiz_socioamb CHAR(18) NOT NULL,
+    cnpj_organiz_socioamb CNPJ_TIPO NOT NULL,
     dt_inicio_acao_co2 DATE NOT NULL,
     nome_acao_co2 TEXT NOT NULL,
 
-    UNIQUE (
-        cnpj_filial_raiz,
-        cnpj_filial_ordem,
-        cnpj_organiz_socioamb,
-        dt_inicio_acao_co2,
-        nome_acao_co2
-    ),
-
-    FOREIGN KEY (cnpj_filial_raiz, cnpj_filial_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem),
-    FOREIGN KEY (cnpj_organiz_socioamb, dt_inicio_acao_co2, nome_acao_co2) REFERENCES acao_co2(cnpj, dt_inicio, nome)
+    CONSTRAINT financia_pk
+        PRIMARY KEY (id),
+    CONSTRAINT financia_sk
+        UNIQUE (
+            cnpj_filial_raiz,
+            cnpj_filial_ordem,
+            cnpj_organiz_socioamb,
+            dt_inicio_acao_co2,
+            nome_acao_co2
+        ),
+    CONSTRAINT financia_fk_acao
+        FOREIGN KEY (cnpj_organiz_socioamb, dt_inicio_acao_co2, nome_acao_co2) REFERENCES acao_co2(cnpj, dt_inicio, nome)
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+    CONSTRAINT finacia_fk_filial
+        FOREIGN KEY (cnpj_filial_raiz, cnpj_filial_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 -- Obs. devido à separação entre as tabelas conforme
@@ -96,11 +165,16 @@ CREATE TABLE vinc_contrib_co2 (
 -- contribuição é válida, sendo necessário fazer a
 -- verificação em triggers SQL ou na aplicação.
 CREATE TABLE contrib_co2 (
-    id_contrib SERIAL REFERENCES vinc_contrib_co2(id) ON DELETE CASCADE,
+    id_contrib SERIAL,
     dt DATE,
     valor DECIMAL NOT NULL,
 
-    PRIMARY KEY (id_contrib, dt)
+    CONSTRAINT contrib_co2_pk
+        PRIMARY KEY (id_contrib, dt),
+    CONSTRAINT contrib_co2_fk
+        FOREIGN KEY (id_contrib) REFERENCES vinc_contrib_co2(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE hist_co2 (
@@ -112,24 +186,34 @@ CREATE TABLE hist_co2 (
     emissao_tot DECIMAL NOT NULL,
     compens_tot DECIMAL NOT NULL,
 
-    PRIMARY KEY (cnpj_raiz, cnpj_ordem, ano, mes),
-    FOREIGN KEY (cnpj_raiz, cnpj_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem)
+    CONSTRAINT hist_co2_pk
+        PRIMARY KEY (cnpj_raiz, cnpj_ordem, ano, mes),
+    CONSTRAINT hist_co2_fk
+        FOREIGN KEY (cnpj_raiz, cnpj_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE serv_nbs (
-    nbs CHAR(12) PRIMARY KEY,
-    nome TEXT
+    nbs CHAR(12),
+    nome TEXT,
+
+    CONSTRAINT serv_nbs_pk
+        PRIMARY KEY(nbs)
 );
 
 CREATE TABLE prod_ncm (
-    ncm CHAR(10) PRIMARY KEY,
-    nome TEXT
+    ncm CHAR(10),
+    nome TEXT,
+
+    CONSTRAINT prod_ncm_pk
+        PRIMARY KEY(ncm)
 );
 
 CREATE TYPE REG_LEG_TIPO AS ENUM ('if', 'multa');
 
 CREATE TABLE reg_leg (
-    ent CHAR(7) REFERENCES ent_fed(cod_ibge),
+    ent COD_IBGE_TIPO,
     tipo REG_LEG_TIPO,
     nro INT,
     ano INT,
@@ -137,8 +221,8 @@ CREATE TABLE reg_leg (
     dt_vigencia DATE NOT NULL,
     dt_revogacao DATE,
 
-    serv CHAR(12) REFERENCES serv_nbs(nbs),
-    prod CHAR(10) REFERENCES prod_ncm(ncm),
+    serv CHAR(12),
+    prod CHAR(10),
 
     lim_multa DECIMAL,
     base_calc_multa DECIMAL,
@@ -146,7 +230,20 @@ CREATE TABLE reg_leg (
     meta_if DECIMAL,
     aliq_if DECIMAL,
 
-    PRIMARY KEY (ent, tipo, nro, ano)
+    CONSTRAINT reg_leg_pk
+        PRIMARY KEY (ent, tipo, nro, ano),
+    CONSTRAINT reg_leg_fk_ent
+        FOREIGN KEY (ent) REFERENCES ent_fed(cod_ibge)
+        ON DELETE RESTRICT 
+        ON UPDATE RESTRICT,
+    CONSTRAINT reg_leg_fk_serv
+        FOREIGN KEY (serv) REFERENCES serv_nbs(nbs)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT reg_leg_fk_prod
+        FOREIGN KEY (prod) REFERENCES prod_ncm(ncm)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 -- Visto que buscas por regras legislativas que estão em vigor em um determinado
@@ -156,67 +253,114 @@ CREATE TABLE reg_leg (
 CREATE INDEX IF NOT EXISTS reg_leg_ent_dt_vigencia_idx ON reg_leg(ent, dt_vigencia);
 
 CREATE TABLE inst_cient (
-    cnpj CHAR(18) PRIMARY KEY,
+    cnpj CNPJ_TIPO,
     nome TEXT NOT NULL,
     end_rua TEXT,
     end_nro INT,
     end_cep CHAR(9),
-    mun_cod CHAR(7) REFERENCES org_adm_mun(cod_ibge)
+    mun_cod COD_IBGE_TIPO,
+
+    CONSTRAINT inst_cient_pk
+        PRIMARY KEY (cnpj),
+    CONSTRAINT inst_cient_fk
+        FOREIGN KEY (mun_cod) REFERENCES org_adm_mun(cod_ibge)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE equipe_inst_cient (
-    cnpj_inst_cient CHAR(18),
+    cnpj_inst_cient CNPJ_TIPO,
     nome TEXT,
     
-    PRIMARY KEY (cnpj_inst_cient, nome)
+    CONSTRAINT equipe_inst_cient_pk 
+        PRIMARY KEY (cnpj_inst_cient, nome),
+    CONSTRAINT equipe_inst_cient_fk
+        FOREIGN KEY (cnpj_inst_cient) REFERENCES inst_cient(cnpj)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE        
 );
 
 CREATE TABLE equipe_inst_cient_membro (
-    cnpj_inst_cient CHAR(18),
+    cnpj_inst_cient CNPJ_TIPO,
     nome_equipe TEXT,
     nome_membro TEXT,
 
-    PRIMARY KEY (cnpj_inst_cient, nome_equipe, nome_membro),
-    FOREIGN KEY (cnpj_inst_cient, nome_equipe) REFERENCES equipe_inst_cient(cnpj_inst_cient, nome)
+    CONSTRAINT equipe_inst_cient_membro_pk
+        PRIMARY KEY (cnpj_inst_cient, nome_equipe, nome_membro),
+    CONSTRAINT equipe_inst_cient_membro_fk
+        FOREIGN KEY (cnpj_inst_cient, nome_equipe) REFERENCES equipe_inst_cient(cnpj_inst_cient, nome)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE relatorio (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     dt_pedido DATE NOT NULL,
     dt_pub DATE,
     
     cnpj_filial_raiz CHAR(10) NOT NULL,
     cnpj_filial_ordem CHAR(7) NOT NULL,
     
-    cnpj_inst_cient CHAR(18) NOT NULL,
+    cnpj_inst_cient CNPJ_TIPO NOT NULL,
     nome_equipe TEXT NOT NULL,
     
-    mun_cod CHAR(7) REFERENCES org_adm_mun(cod_ibge),
+    mun_cod COD_IBGE_TIPO REFERENCES org_adm_mun(cod_ibge),
     
     aliq_if DECIMAL,
     multa_aplic DECIMAL,
 
-    UNIQUE (dt_pedido, cnpj_filial_raiz, cnpj_filial_ordem, cnpj_inst_cient, nome_equipe),
-    FOREIGN KEY (cnpj_filial_raiz, cnpj_filial_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem),
-    FOREIGN KEY (cnpj_inst_cient, nome_equipe) REFERENCES equipe_inst_cient(cnpj_inst_cient, nome)
+    CONSTRAINT relatorio_pk
+        PRIMARY KEY(id),
+    CONSTRAINT relatorio_sk
+        UNIQUE (
+            dt_pedido, 
+            cnpj_filial_raiz, cnpj_filial_ordem, 
+            cnpj_inst_cient, nome_equipe
+        ),
+    CONSTRAINT relatorio_fk_filial
+        FOREIGN KEY (cnpj_filial_raiz, cnpj_filial_ordem) REFERENCES filial(cnpj_raiz, cnpj_ordem)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    CONSTRAINT relatorio_fk_inst_cient
+        FOREIGN KEY (cnpj_inst_cient, nome_equipe) REFERENCES equipe_inst_cient(cnpj_inst_cient, nome)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE relatorio_prod (
-    id_relatorio SERIAL REFERENCES relatorio(id),
-    ncm CHAR(10) REFERENCES prod_ncm(ncm),
+    id_relatorio SERIAL,
+    ncm CHAR(10),
     tco2_p_un DECIMAL NOT NULL,
     qtde DECIMAL NOT NULL,
 
-    PRIMARY KEY (id_relatorio, ncm)
+    CONSTRAINT relatorio_prod_pk
+        PRIMARY KEY (id_relatorio, ncm),
+    CONSTRAINT relatorio_prod_fk_relatorio
+        FOREIGN KEY (id_relatorio) REFERENCES relatorio(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT relatorio_prod_fk_prod
+        FOREIGN KEY (ncm) REFERENCES prod_ncm(ncm)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE relatorio_serv (
-    id_relatorio SERIAL REFERENCES relatorio(id),
-    nbs CHAR(12) REFERENCES serv_nbs(nbs),
+    id_relatorio SERIAL,
+    nbs CHAR(12),
     ocorrencia TIMESTAMPTZ,
     tco2 DECIMAL NOT NULL,
 
-    PRIMARY KEY (id_relatorio, nbs, ocorrencia)
+    CONSTRAINT relatorio_serv_pk
+        PRIMARY KEY (id_relatorio, nbs, ocorrencia),
+    CONSTRAINT relatorio_serv_fk_relatorio
+        FOREIGN KEY (id_relatorio) REFERENCES relatorio(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT relatorio_serv_fk_serv
+        FOREIGN KEY (nbs) REFERENCES serv_nbs(nbs)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 \i db/filters/reg_leg.sql;
