c3a7805535657557d43a2babad2d97f0e2e21257
conti <contimarcelao@gmail.com>
2026-06-20 15:42:57 -0300

    documente a query de filiais sem multa

--- db/queries.sql
+++ db/queries.sql
@@ -1,28 +1,5 @@
 DEALLOCATE ALL;
 
--- N filiais com o menor histórico de emissão de carbono nos últimos K meses.
-
--- Todas as filiais que contribuíram mais do que R$ 500.000,00 em ações de compensação
--- nos últimos K meses, sendo pelo menos 30% de suas contribuições na área de energia.
-
--- Todas as filiais que não receberam nenhuma multa no último ano.
-
--- Todas as filiais que possuem dois ou menos relatórios sobre si gerados no último ano.
-
--- Todas as filiais que foram investigadas por três ou mais instituições científicas
--- diferentes nos últimos dois anos.
-
--- Todas as filiais com saldo de emissão positivo nos últimos Y anos, incluindo o valor total
--- investido em cada área das ações de compensação.
-
--- (complexa)
--- Todas as filiais cujas emissões nos últimos X meses foram oriundas
--- predominantemente (> 50%) de um único tipo de produto ou serviço.
-
--- Para todas as Instituições Cientificas do Rio de Janeiro, listar a média do histórico
--- de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
-
--- (útil)
 -- Todas as regras legislativas que se aplicam a um certo município.
 --
 -- Argumentos:
@@ -34,22 +11,24 @@ PREPARE reg_leg_mun(TEXT, CHAR(2)) AS
     )
     SELECT * FROM reg_leg, cod WHERE ent = cod.uf OR ent = cod.mun; 
 
--- (útil)
--- Todas as regras legislativas que se aplicam a uma determinada filial, em um determinado mês.
 
--- Todas as empresas que financiaram alguma ação de compensação de uma determinada
--- organização socioambiental, incluindo o valor total contribuído por cada empresa.
+-- Todas as filiais que não receberam nenhuma multa em um intervalo
+-- de datas passadas por parâmetro   
+--
+-- Argumentos:
+--  - Intervalo de data, na qual queira saber quais filiais levaram multa
+--
+-- Assumindo:
+--  - Foi adotado que a Instituição de pesquisa começa a analisar determinada
+--    filial, após um mês da data de pedido
+-- 
+--  - Foi adotado que a Instituição de pesquisa demora entorno de 1 mês para
+--    terminar sua análise de determinada filial
+--
+--  - Foi adotado que qualquer relatório que tiver um valor de multa, em
+--    que o periodo da analise conter sobreposição com o periodo passado por 
+--    parâmetro, é considerado uma tupla falsa para a query
 
--- As top K filiais que mais investiram em ações de compensação, agrupadas por
--- área da ação.
-
--- Todas as regras legislativas de incentivo fiscal cujas metas não foram atingidas por
--- nenhuma filial nos últimos X meses.
-
--- Todas as regras legislativas de incentivo fiscal cujas metas foram alcançadas por pelo
--- menos uma filial todo mês, em um período de três meses subsequentes.
-
--- 1) Todas as filiais que não receberam nenhuma multa em um intervalo
 PREPARE filial_sem_multa_p(DATERANGE) AS
     SELECT cnpj_raiz, cnpj_ordem FROM filial
         EXCEPT 
@@ -67,11 +46,11 @@ PREPARE filial_sem_multa_p(DATERANGE) AS
             )
             AND r.multa_aplic > 0;
 
--- 2) Todas as filiais que contribuíram mais do que $"R$X"$ em ações de compensação, nos últimos K meses, sendo pelo menos Y% de suas contribuições em uma area Z
+-- Todas as filiais que contribuíram mais do que $"R$X"$ em ações de compensação, nos últimos K meses, sendo pelo menos Y% de suas contribuições em uma area Z
 
--- 3) Todas as filiais cujas emissões nos últimos X meses foram oriundas majoritariamente de um único tipo de produto ou serviço.
+-- Todas as filiais cujas emissões nos últimos X meses foram oriundas majoritariamente de um único tipo de produto ou serviço.
 -- PREPARE filiais_emissao_concentrada_k(DECIMAL, INT)
 
--- 4) Todas as regras legislativas de incentivo fiscal cujas metas não foram atingidas por nenhuma filial nos últimos X meses
+-- Todas as regras legislativas de incentivo fiscal cujas metas não foram atingidas por nenhuma filial nos últimos X meses
 
--- 5) Para todas as Instituições Cientificas do município X, listar a média do histórico de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
+-- Para todas as Instituições Cientificas do município X, listar a média do histórico de CO2 de cada empresa avaliadas por suas equipes, ordenadas pela média do histórico.
