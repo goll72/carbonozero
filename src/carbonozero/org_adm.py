e05bba0195340bbc7b50de4f43583179b0ac1a50
conti <contimarcelao@gmail.com>
2026-06-21 15:03:01 -0300

    fix bug

--- src/carbonozero/org_adm.py
+++ src/carbonozero/org_adm.py
@@ -2,14 +2,14 @@ from rich.console import Console
 from rich.prompt import Prompt
 from rich.panel import Panel
 
-from .common import text_bar
+from .common import text_bar, conn_status
 
 import asyncpg
 
 
 async def menu_org_adm(console: Console, conn: asyncpg.Connection):
     while True:
-        console.print(text_bar("CarbonoZero --- Inst. Cient.", console.width, aside=conn_status(conn)))
+        console.print(text_bar("CarbonoZero --- Org. Adm.", console.width, aside=conn_status(conn)))
         
         menu = Panel(
             "\n".join([
